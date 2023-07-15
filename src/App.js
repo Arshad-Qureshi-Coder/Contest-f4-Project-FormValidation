d3c3ab8c60f58cca2c2e3edb07f876f4b72a6d1d
Arshad Qureshi <arshu080@gmail.com>
2023-07-15 19:55:14 +0530

    Added my project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AccountForm from "./Component/AccountForm";
+
+
+
+const App = () => {
+
+    return (
+        <div>
+            <AccountForm/>
+        </div>
+    )
+}
+
+export default App;
